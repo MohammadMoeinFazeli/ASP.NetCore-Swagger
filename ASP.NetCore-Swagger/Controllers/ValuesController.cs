50de4790af18d3198404ddf87aae18a2c9a4cf1f
root <you@example.com>
2019-07-01 17:14:58 +0430

    init commit

---
+++ ASP.NetCore-Swagger/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCore_Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// Get action method without any argument
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] {"value1", "value2"};
+        }
+
+        /// <summary>
+        /// Get action method that accepts an integer as an argument
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Post action method to add data
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// Put action method to modify data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// Delete action method
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ASP.NetCore-Swagger/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ASP.NetCore_Swagger
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Swagger service properties
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Swagger Demo",
+                    Description = "Swagger Demo - angular-netcore.ir",
+                    TermsOfService = "None",
+                    Contact = new Contact()
+                    {
+                        Name = "Mohammad Moein Fazeli",
+                        Email = "mmfazeli372@gmail.com",
+                        Url = "http://www.angular-netcore.ir"
+                    }
+                });
+
+                // locate the XML file
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, "XMLdocument.xml");
+
+                // config swagger to use those XML comments.
+                c.IncludeXmlComments(xmlPath);
+            });
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+    }
+}
